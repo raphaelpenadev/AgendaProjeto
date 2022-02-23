6d782baf8f129694fd0c2bfefeb375ecd99627e1
unknown <raphael.pena@fadesp.org.br>
2022-02-23 18:07:42 -0300

    Link de dependencias

--- index.php
+++ index.php
@@ -10,11 +10,17 @@ include_once("config/url.php");
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Agenda Projeto</title>
+  <!-- CSS PROPRIO -->
   <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+  <!-- BOOTSTRAP 4.6 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- FONT AWESOME -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-
+  <h1>Testando Agenda</h1>
+  <i class="fas fa-eye"></i>
 </body>
 
 </html>
